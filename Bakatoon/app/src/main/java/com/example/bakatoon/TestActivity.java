31de96356a17840e21f658ea8fb13fbff336b068
Raflidikap <raflidikapramudya7@gmail.com>
2022-09-22 21:43:13 +0700

    button

--- Bakatoon/app/src/main/java/com/example/bakatoon/TestActivity.java
+++ Bakatoon/app/src/main/java/com/example/bakatoon/TestActivity.java
@@ -6,9 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class TestActivity extends AppCompatActivity {
     private Button backBtn;
+    private RadioGroup radioGroup;
+    private RadioButton radioButton;
+    private TextView textView, textView1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +23,10 @@ public class TestActivity extends AppCompatActivity {
         setContentView(R.layout.activity_test);
 
         backBtn = findViewById(R.id.backBtn);
-
+        radioGroup = findViewById(R.id.btnGroup1);
+        textView = findViewById(R.id.tv_test1);
+        textView1 = findViewById(R.id.tv_test2);
+        Button buttonSubmit = findViewById(R.id.submitBtn);
 
         //set tombol back ke main activities
         backBtn.setOnClickListener(new View.OnClickListener() {
@@ -26,5 +36,19 @@ public class TestActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int radioId = radioGroup.getCheckedRadioButtonId();
+                radioButton = findViewById(radioId);
+            }
+        });
+    }
+
+    public void checkButton(View v){
+        int radioId = radioGroup.getCheckedRadioButtonId();
+        radioButton = findViewById(radioId);
+        Toast.makeText(this, "selected radio button: " + radioButton.getText(), Toast.LENGTH_SHORT).show();
     }
 }
