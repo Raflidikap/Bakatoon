3c70c0758b9a133e83a893d5bc399c321abaf3c4
Raflidikap <raflidikapramudya7@gmail.com>
2022-10-24 14:36:17 +0700

    change ic launcher

--- Bakatoon/app/src/main/java/com/example/bakatoon/LoginActivity.java
+++ Bakatoon/app/src/main/java/com/example/bakatoon/LoginActivity.java
@@ -89,6 +89,7 @@ public class LoginActivity extends AppCompatActivity {
                             public void onDataChange(@NonNull DataSnapshot snapshot) {
                                 GlobalVar.currentUser = snapshot.getValue(User.class);
                                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
                                 startActivity(intent);
                                 finish();
                             }
@@ -132,6 +133,7 @@ public class LoginActivity extends AppCompatActivity {
                 public void onDataChange(@NonNull DataSnapshot snapshot) {
                     GlobalVar.currentUser = snapshot.getValue(User.class);
                     Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
                     startActivity(intent);
                     finish();
                 }
